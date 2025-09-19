42ad48b38a79aba56adc52a92bfb41e2fd62da9d
goddybrilliaant Khowane <mnqobiddybrilliantkhowane@gmail.com>
2025-09-19 20:44:44 +0200

    Add files via upload

---
+++ Coin Toss Application/Coin Toss Application/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Coin_Toss_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbTail = new System.Windows.Forms.PictureBox();
+            this.pbHead = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnToss = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbTail
+            // 
+            this.pbTail.Image = ((System.Drawing.Image)(resources.GetObject("pbTail.Image")));
+            this.pbTail.Location = new System.Drawing.Point(139, 110);
+            this.pbTail.Name = "pbTail";
+            this.pbTail.Size = new System.Drawing.Size(157, 146);
+            this.pbTail.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTail.TabIndex = 0;
+            this.pbTail.TabStop = false;
+            // 
+            // pbHead
+            // 
+            this.pbHead.Image = ((System.Drawing.Image)(resources.GetObject("pbHead.Image")));
+            this.pbHead.Location = new System.Drawing.Point(139, 110);
+            this.pbHead.Name = "pbHead";
+            this.pbHead.Size = new System.Drawing.Size(163, 146);
+            this.pbHead.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbHead.TabIndex = 1;
+            this.pbHead.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coin Toss Game";
+            // 
+            // btnToss
+            // 
+            this.btnToss.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToss.Location = new System.Drawing.Point(107, 301);
+            this.btnToss.Name = "btnToss";
+            this.btnToss.Size = new System.Drawing.Size(75, 36);
+            this.btnToss.TabIndex = 3;
+            this.btnToss.Text = "Toss";
+            this.btnToss.UseVisualStyleBackColor = true;
+            this.btnToss.Click += new System.EventHandler(this.btnToss_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(235, 301);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 36);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(443, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnToss);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbHead);
+            this.Controls.Add(this.pbTail);
+            this.Name = "Form1";
+            this.Text = "Coin Toss";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHead)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTail;
+        private System.Windows.Forms.PictureBox pbHead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnToss;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Coin Toss Application/Coin Toss Application/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coin_Toss_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToss_Click(object sender, EventArgs e)
+        {
+            //Variable to indicate which side is up.
+            int sideUp;
+
+            //Create a Random object.
+            Random rand = new Random();
+
+            //Get a random integer in the range of 0 through 1.
+            // 0 means tails up & 1 means heads up.
+            sideUp = rand.Next(2);
+
+            //Display the side that is up.
+            if(sideUp == 0)
+            {
+                //Display tails up.
+                pbTail.Visible = true;
+                pbHead.Visible = false;
+            }
+
+            else
+            {
+                //Display the heads up.
+                pbHead.Visible = true;
+                pbTail.Visible = false;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the form or end the game.
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //The warm welcome message to play the game.
+            MessageBox.Show("WELCOME CLICK THE OK BUTTON TO START PLAYING BUDDY!");
+        }
+    }
+}
